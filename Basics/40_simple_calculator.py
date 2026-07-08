3e5cf203afd4dafeec6dcc97f5cd80db7bdac4de
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-08 22:16:16 +0530

    Create 40_simple_calculator.py

---
+++ Basics/40_simple_calculator.py
@@ -0,0 +1,18 @@
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+
+add=n1+n2
+sub=n1-n2
+mul=n1*n2
+div=n1/n2
+floor_division=n1//n2
+mod=n1%n2
+exponent=n1**n2
+
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
+print("Floor division(Quotient):",floor_division)
+print("Modulus(Remainder):",mod)
+print("Exponent:",exponent)
