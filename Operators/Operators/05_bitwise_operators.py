a456625607e2e505a208e85656fee631950a1c56
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-09 22:03:49 +0530

    Add bitwise operators demonstration in Python

---
+++ Operators/Operators/05_bitwise_operators.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Bitwise AND =", num1 & num2)
+print("Bitwise OR =", num1 | num2)
+print("Bitwise XOR =", num1 ^ num2)
+print("Bitwise NOT of num1 =", ~num1)
+print("Left Shift =", num1 << 1)
+print("Right Shift =", num1 >> 1)
