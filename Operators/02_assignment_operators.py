f7f3f4c32ed50cbd2993d4f19ea1fc66312a596f
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-09 15:06:09 +0530

    Create 02_assignment_operators.py

---
+++ Operators/02_assignment_operators.py
@@ -0,0 +1,30 @@
+a=10
+print("Initial value of a=",a)
+
+#+=
+a+=5
+print("The value of a after +=5 is",a)
+
+#-=
+a-=5
+print("The value of a after -=5 is",a)
+
+#*=
+a*=5
+print("The value of a after *=5 is",a)
+
+#/=
+a/=5
+print("The value of a after /=5 is",a)
+
+#%= [ a=a % 3 then 6.0 ÷ 3 = 2 remainder 0 so,a=0.0 ]
+a %= 3
+print("The value of a after %=3 is",a)
+
+#//=
+a//=5
+print("The value of a after //=5 is",a)
+
+#**=
+a**=2
+print("The value of a after **=5 is",a)
