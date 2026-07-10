06c256a01a87a39793d78794f5e6cb9bfcda8a30
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-10 16:54:22 +0530

    Create 09_arithmetic_assignment_combination.py

---
+++ OPERATORS/09_arithmetic_assignment_combination.py
@@ -0,0 +1,16 @@
+num=int(input("Enter a number:"))
+
+num+=10
+print("num After +=10 is",num)
+
+num-=10
+print("num After -=10 is",num)
+
+num*=10
+print("num After *=10 is",num)
+
+num/=10
+print("num After /=10 is",num)
+
+num**=2
+print("num After **=2 is",num)
