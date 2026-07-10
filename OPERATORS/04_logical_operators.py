93105d5198dcf1a815e6977217cd0b1e4fe82250
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-10 13:23:45 +0530

    Add logical operators demonstration in Python

---
+++ OPERATORS/04_logical_operators.py
@@ -0,0 +1,10 @@
+num=int(input("Enter the number:"))
+
+print("AND Operator")
+print("Greater than 0 and less than 100:", num > 0 and num < 100)
+
+print("OR Operator")
+print("Less than 0 or greater than 100:", num < 0 or num > 100)
+
+print("NOT Operator")
+print("Not equal to 50:", not (num == 50))
