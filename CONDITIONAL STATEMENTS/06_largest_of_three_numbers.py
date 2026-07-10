5ae8b59916c3343cfc8cb753c5031897437e4ccf
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-10 22:50:01 +0530

    Modify largest number logic to include equality check

--- CONDITIONAL STATEMENTS/06_largest_of_three_numbers.py
+++ CONDITIONAL STATEMENTS/06_largest_of_three_numbers.py
@@ -2,9 +2,9 @@ num1=float(input("Enter first number:"))
 num2=float(input("Enter second number:"))
 num3=float(input("Enter third number:"))
 
-if num1>num2 and num1>num3:
+if num1>=num2 and num1>=num3:    # n1>=n2 and n1>=n3
     print(f"{num1} is largest")
-elif num2>num1 and num2>num3:
+elif num2>=num1 and num2>=num3:
     print(f"{num2} is largest")
 else:
     print(f"{num3} is largest")
