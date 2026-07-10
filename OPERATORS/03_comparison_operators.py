eb59aa68a74c7e22e67c894b2032849334bc996c
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-10 13:23:04 +0530

    Add comparison operators demonstration script

---
+++ OPERATORS/03_comparison_operators.py
@@ -0,0 +1,13 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("The value of a is",a)
+print("The value of b is",b)
+print("Comparison Operator")
+
+print("a==b:",a==b)
+print("a!=b:",a!=b)
+print("a<b:",a<b)
+print("a>b:",a>b)
+print("a<=b:",a<=b)
+print("a>=b:",a>=b)
