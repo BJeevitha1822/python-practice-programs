ba3a209484875801ceec2651bced7d7168cc5752
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-11 22:47:35 +0530

    Create 32_find_roots_of_quadratic_equation.py

---
+++ CONDITIONAL STATEMENTS/32_find_roots_of_quadratic_equation.py
@@ -0,0 +1,18 @@
+import math
+a = float(input("Enter coefficient a: "))
+b = float(input("Enter coefficient b: "))
+c = float(input("Enter coefficient c: "))
+
+discriminant = b ** 2 - 4 * a * c
+
+if discriminant > 0:
+    root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    root2 = (-b - math.sqrt(discriminant)) / (2 * a)
+    print("Root 1 =", root1)
+    print("Root 2 =", root2)
+elif discriminant == 0:
+    root = -b / (2 * a)
+    print("Both roots are equal.")
+    print("Root =", root)
+else:
+    print("The roots are imaginary.")
