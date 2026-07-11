a3242a9c8a36d74d4215d5a39d51df4a66c39ff0
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-11 17:19:12 +0530

    Create 22_check_divisibility.py

---
+++ CONDITIONAL STATEMENTS/22_check_divisibility.py
@@ -0,0 +1,10 @@
+num=float(input("Enter the number:"))
+
+if num%3==0 and num%5==0:
+    print(num,"is divisible by both 3 and 5")
+elif num%3==0:
+    print(num,"is divisible by only 3")
+elif num%5==0:
+    print(num,"is divisible by only 5")
+else:
+    print(num,"is not divisible by 3 or 5")
