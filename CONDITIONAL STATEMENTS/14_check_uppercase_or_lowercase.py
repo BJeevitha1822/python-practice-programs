741de8d49b457eccdaeaea6a4562dbafb96e69b8
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-11 09:43:53 +0530

    Add function to check if character is uppercase or lowercase

---
+++ CONDITIONAL STATEMENTS/14_check_uppercase_or_lowercase.py
@@ -0,0 +1,7 @@
+char=input("Enter a character:")
+if 'A'<=char<='Z':
+    print(char,"is an uppercase letter")
+elif 'a'<=char<='z':
+    print(char,"is a lowercase letter")
+else:
+    print("Invalid input")
