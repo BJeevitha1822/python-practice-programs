1f02bfd4ad3ae37be433439c230491d1c6bd1032
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-13 17:12:37 +0530

    Update 05_function_with_default_arguments.py

--- Functions/05_function_with_default_arguments.py
+++ Functions/05_function_with_default_arguments.py
@@ -4,3 +4,8 @@ greet()
 
 user_name=input("Enter your name:")
 greet(user_name)
+
+'''
+^Default parameters in Python allow you to define a function with default values for its arguments. 
+^If no value is provided during the function call, the default value is used.
+'''
