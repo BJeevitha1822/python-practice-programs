46d7c90e2009423cca1124aec1408e105c69629a
BJeevitha1822 <bjeevi1822@gmail.com>
2026-07-13 17:03:46 +0530

    Update 06_function_with_keyword_arguments.py

--- Functions/06_function_with_keyword_arguments.py
+++ Functions/06_function_with_keyword_arguments.py
@@ -2,3 +2,10 @@ def student_details(name,age):
     print("Name:",name)
     print("Age:",age)
 student_details(name="Jeevitha",age=18)
+
+'''
+^Keyword arguments are arguments passed to a function by explicitly specifying the parameter name along with its value.
+^In the call student_details(name="Jeevitha", age=18):
+     *name is the parameter, "Jeevitha" is the value.
+     *age is the parameter, 18 is the value
+'''
